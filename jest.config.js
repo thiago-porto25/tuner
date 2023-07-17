37afe85ba9b5bc26516f5281cf0035dd61773e84
Thiago Porto <thiago.porto@igma.do>
2023-07-17 15:10:11 -0300

    chore: setup testing-library, setup coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,21 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
   },
 };
