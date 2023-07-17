375f3380bf43673f31ac7dfc5ba5fdabff8ba7a0
Thiago Porto <thiago.porto@igma.do>
2023-07-17 20:04:38 -0300

    chore: setup internationalization with i18n-js
    setup i18n-js and react-native-localize to support different languages

---
+++ src/shared/i18n/supportedLanguages.ts
@@ -0,0 +1,8 @@
+const en = {
+  welcome: 'Internationalization and Localization in React Native',
+};
+const pt = {
+  welcome: 'Internacionalização e Localização em React Native',
+};
+
+export { en, pt };
