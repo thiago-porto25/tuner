f883b4962d693a3d74b27b97306b87fe52c22cdb
Thiago Porto <thiago.porto@igma.do>
2023-07-17 15:10:11 -0300

    chore: setup testing-library, setup coverage

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,29 @@
-import 'react-native';
 import React from 'react';
+import reactNative from 'react-native';
 
+import { render, screen } from '@testing-library/react-native';
 import renderer from 'react-test-renderer';
 
 import App from '@/App';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const useColorSchemeSpy = jest.spyOn(reactNative, 'useColorScheme');
+
+describe('App', () => {
+  it('renders correctly', () => {
+    renderer.create(<App />);
+  });
+
+  it('renders correctly with testing library', () => {
+    render(<App />);
+
+    expect(screen.getAllByText(/React Native/i, { exact: false })).toBeTruthy();
+  });
+
+  it('renders correctly with testing library and dark mode', () => {
+    useColorSchemeSpy.mockReturnValueOnce('dark');
+
+    render(<App />);
+
+    expect(screen.getAllByText(/React Native/i, { exact: false })).toBeTruthy();
+  });
 });
