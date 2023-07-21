22bce8c98f15540dbf6873fa8747ab145c804cfe
Thiago Porto <thiago.porto@igma.do>
2023-07-21 18:25:07 -0300

    chore: rename test files and dirs, create new type

--- src/__tests__/test-utils/setupTests.ts
+++ src/__tests__/test-utils/setupTests.ts
@@ -4,6 +4,7 @@ import * as mockRNLocalize from 'react-native-localize/mock';
 jest.mock('react-native-localize', () => mockRNLocalize);
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('i18n-js', () => ({
+  __esModule: true,
   I18n: () => {
     return {
       t: jest.fn((str: string) => str),
