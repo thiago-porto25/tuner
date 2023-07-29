218b3f55bb6c02cf1da523bfec98132264242084
Thiago Porto <thiago.porto@igma.do>
2023-07-29 17:55:04 -0300

    chore: install correct recording and pitch packages
    Also update jest config, eslintrc, add a module declaration for react-native-recording, and remove
    uneeded permissions in AndroidManifest

---
+++ react-native-recording.d.ts
@@ -0,0 +1,17 @@
+declare module 'react-native-recording' {
+  interface options {
+    bufferSize: number;
+    sampleRate: number;
+    bitsPerChannel: 8 | 16;
+    channelsPerFrame: 1 | 2;
+  }
+  declare const _default: {
+    init: (options: options) => any;
+    start: () => any;
+    stop: () => any;
+    addRecordingEventListener: (
+      listener: (data: Float32Array) => void,
+    ) => import('react-native').EmitterSubscription;
+  };
+  export default _default;
+}
